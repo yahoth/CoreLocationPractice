5e282bc95fb1e89171e786f9a5475c4c4dda00b6
TaeHyoung Kim <99034396+yahoth@users.noreply.github.com>
2024-02-02 03:51:46 +0900

    [Implement] Log view 구현하여 visit 메소드 호출될 때 로그에 보이도록 구성함, 백그라운드 모드 추가

--- CoreLocationPractice/LocationManager.swift
+++ CoreLocationPractice/LocationManager.swift
@@ -8,46 +8,49 @@
 import Foundation
 import CoreLocation
 
+struct Log: Hashable {
+    let id: UUID = UUID()
+    let text: String
+}
+
 class LocationManager: NSObject {
     let locationManager = CLLocationManager()
     var previousLocation: CLLocation?
+    @Published var logs: [Log] = []
 
     override init() {
         super.init()
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.showsBackgroundLocationIndicator = true
     }
 
     func start() {
-        print("*** Start Updating Location ***\n")
-        locationManager.startUpdatingLocation()
+        locationManager.startMonitoringVisits()
+        logs.append(Log(text: "*** Start MonitoringVisits ***"))
     }
 
     func stop() {
-        print("*** Stop Updating Location ***")
-        locationManager.stopUpdatingLocation()
+        locationManager.stopMonitoringVisits()
+        logs.append(Log(text: "*** Start MonitoringVisits ***"))
 
     }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        let coordinate = location.coordinate
-        let speed = location.speed
-        let horizontalAccuracy = location.horizontalAccuracy
-        let speedAccuracy = location.speedAccuracy
-        print("--------------------------------------------------------------------------------------\n")
-        print("Coordinate: \(coordinate), accuracy: \(horizontalAccuracy)\n")
-        print("Speed: \(Int(speed))M, accuracy: \(speedAccuracy)\n")
-        if let previousLocation {
-            let time = location.timestamp.timeIntervalSince(previousLocation.timestamp)
-            print("period: \(time)")
+    func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
+        if visit.departureDate == Date.distantPast {
+            logs.append(Log(text: "방문 시작: \(visit.arrivalDate)"))
+        } else {
+            logs.append(Log(text: "방문 종료: \(visit.departureDate)"))
         }
-        previousLocation = location
     }
 
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
+        logs.append(Log(text: error.localizedDescription))
+    }
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print(error)
+        logs.append(Log(text: error.localizedDescription))
     }
 }
--- CoreLocationPractice/ViewController.swift
+++ CoreLocationPractice/ViewController.swift
@@ -7,57 +7,148 @@
 
 import UIKit
 import CoreLocation
-import SnapKit
+import Combine
 
 class ViewController: UIViewController {
 
     let locationManager = LocationManager()
     var startButton: UIButton!
     var stopButton: UIButton!
+    var hStackView: UIStackView!
+    var collectionView: UICollectionView!
+//    var log: [String] = []
+    var datasource: UICollectionViewDiffableDataSource<Section, Log>!
+    var subscriptions = Set<AnyCancellable>()
+    enum Section {
+        case main
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setCollectionView()
+        configureDatasource()
+        setHStackView()
         setButton()
         setConstraints()
+        bind()
+        setNavigationItem()
+    }
+
+    func applySnapshot(logs: [Log]) {
+        var snapshot = datasource.snapshot()
+        snapshot.deleteAllItems()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(logs)
+        datasource.apply(snapshot)
+    }
+
+    func bind() {
+        locationManager.$logs
+            .receive(on: DispatchQueue.main)
+            .sink { logs in
+                self.applySnapshot(logs: logs)
+            }.store(in: &subscriptions)
     }
 }
 
-//UI관련 코드
+
+// CollectionView 관련 코드
 extension ViewController {
+
+    func setCollectionView() {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.backgroundColor = .clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    func configureDatasource() {
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Log> { cell, indexPath, item in
+            var content = cell.defaultContentConfiguration()
+            content.text = item.text
+            cell.contentConfiguration = content
+        }
+
+        datasource = UICollectionViewDiffableDataSource(collectionView: collectionView) { collectionView, indexPath, item in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
+        }
+
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Log>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems([])
+        datasource.apply(snapshot)
+    }
+
+    func createLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+}
+
+//UI Components 관련 코드
+extension ViewController {
+
+
+    func setHStackView() {
+        hStackView = UIStackView()
+        hStackView.axis = .horizontal
+        hStackView.spacing = 20
+        hStackView.distribution = .fillEqually
+        hStackView.translatesAutoresizingMaskIntoConstraints = false
+    }
+
     func setButton() {
         startButton = UIButton()
         startButton.setTitle("Start", for: .normal)
         startButton.backgroundColor = .blue
         startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
-
         stopButton = UIButton()
         stopButton.setTitle("Stop", for: .normal)
         stopButton.backgroundColor = .red
         stopButton.addTarget(self, action: #selector(stopButtonTapped), for: .touchUpInside)
+
+        [startButton, stopButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.layer.cornerRadius = 30
+        }
     }
 
     func setConstraints() {
-        view.addSubview(startButton)
-        view.addSubview(stopButton)
-        startButton.snp.makeConstraints { make in
-            make.centerY.equalTo(view).offset(-30)
-            make.horizontalEdges.equalTo(view).inset(50)
-            make.height.equalTo(50)
-        }
+        view.addSubview(collectionView)
+        view.addSubview(hStackView)
+        [startButton, stopButton].forEach(hStackView.addArrangedSubview(_:))
 
-        stopButton.snp.makeConstraints { make in
-            make.centerY.equalTo(view).offset(30)
-            make.horizontalEdges.equalTo(view).inset(50)
-            make.height.equalTo(50)
-        }
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: hStackView.topAnchor),
+
+            hStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            hStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            hStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            hStackView.heightAnchor.constraint(equalToConstant: 150)
+        ])
     }
+
     @objc func startButtonTapped() {
         locationManager.start()
     }
     @objc func stopButtonTapped() {
         locationManager.stop()
     }
-
 }
 
+extension ViewController {
+    func setNavigationItem() {
+        let resetItem = UIBarButtonItem(image: UIImage(systemName: "trash"), style: .plain, target: self, action: #selector(resetLog))
+        self.navigationItem.rightBarButtonItem = resetItem
+    }
+
+    @objc func resetLog() {
+        locationManager.logs = []
+    }
+}
